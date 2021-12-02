920456b4fa43e04a41df7270a58cd7d205ea874e
NieDaSie <maksym.malicki.2115@gmail.com>
2021-12-02 14:36:24 +0100

    added redirect addon

---
+++ url-switcher/OdraCentrum-redirect.js
@@ -0,0 +1,31 @@
+// console.log("Przekierowanie nastąpi za 30 sekund")
+// window.setTimeout(function(){
+    // window.location.href = "http://127.0.0.1:8000/"
+// }, 30000)
+
+var id;
+
+function redirectAfterIdle(){
+    console.log("wystartowano zegar!")
+    id = window.setTimeout(function(){
+        window.location.href = "http://127.0.0.1:8000/"
+    }, 30000)
+}
+
+function resetWait(){
+    clearTimeout(id)
+    redirectAfterIdle();
+}
+
+function setWait(){
+    document.addEventListener("touchstart", resetWait, false);
+    document.addEventListener("touchend", resetWait, false);
+    document.addEventListener("touchcancdocument", resetWait, false);
+    document.addEventListener("touchmove", resetWait, false);
+    document.addEventListener("wheel", resetWait, false);
+    document.addEventListener("click", resetWait, false)
+}
+
+redirectAfterIdle()
+setWait()
+
